ee4fc66616ddd37c601367c81d032aaf873f21e4
karen <kdescobar21@gmail.com>
2022-01-31 10:11:35 -0300

    filtro AZ

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterdatadirector} from './data.js';
+import { filterdatadirector, sortData} from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const movies = data.films;
@@ -7,7 +7,7 @@ const container = document.getElementById("films.anime");
 //const  boxModal = document.getElementsByClassName("modal");
 //const filterFilms = document.getElementById("selectFilm");
 
-//const filterordenAZ=document.getElementById("selectordenAZ");
+const filterordenAZ=document.getElementById("selectordenAZ");
 //const filteryear=document.getElementById("selectYear");
 
 
@@ -32,9 +32,35 @@ boxImage(films);
 
 //filtro director-->
 const filterdirector= document.getElementById("selectdirector");
+const filterordenAZ= document.getElementById("selectordenAZ");
 filterdirector.addEventListener("change", (event) => {
-  let selectedDirect = filterdatadirector(movies, event.target.value);
+  let selectedDirect = filterdatadirector(movies, event.target.value,filterordenAZ.value);
   films = selectedDirect;
   boxImage(selectedDirect);
   addEvents();
 });
+
+filterordenAZ.addEventListener("change", (event) => {
+  let selectorden = filterdatadirector(movies, event.target.value,filterordenAZ.value);
+  films = selectorden;
+  boxImage(selectorden);
+  addEvents();
+});
+
+
+
+/*//filtro orden
+const arraymovies  = [sortData];
+arraymovies.sort((a,b) => {a = a.toLowercase().title;
+  
+  b = b.toLowercase().title;
+  if (a > b){ 
+    return 0;
+  }
+  if (a < b){
+    return -1;
+  }
+  console.log(arraymovies);
+})
+ 
+*/
